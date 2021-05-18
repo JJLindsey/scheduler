55d78c9a68c017ae53e149ddcb8826070abe3ef6
JJLindsey <jlindsey010@gmail.com>
2021-05-18 16:22:43 -0500

    add hour rows

---
+++ Assets/js/script.js
@@ -0,0 +1,15 @@
+var saveBtn = $('#button');
+
+var interval = setInterval(hourUpdate, 15000);
+
+// function hourUpdate() {
+//     var currentHour = moment().hours();
+// }
+
+//$('.date').text(moment().format('dddd, MMMM Do'));
+
+// get current date
+var currentDate = new Date();
+
+
+//items to localStorage
