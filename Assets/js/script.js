3df520fefca6477c4409aa92403674f012db00ae
JJLindsey <jlindsey010@gmail.com>
2021-05-19 09:58:51 -0500

    add js for localstorage incomplete

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,3 @@
-var saveBtn = $('#button');
 
 var interval = setInterval(hourUpdate, 15000);
 
@@ -13,3 +12,29 @@ var currentDate = new Date();
 
 
 //items to localStorage
+var saveTask = document.querySelector("#saveBtn");
+
+var entry = JSON.parse(localStorage.setItem('usertask')) || [];
+
+//define function 
+function saveTask(){
+    
+}
+//event listener
+$("#saveBtn").on("click", function(event) {
+    event.preventDefault();
+
+    var toDoEntry = $(".form-control").val().trim();
+    //console.log(event);
+
+    //define 'entry' variabel earlier
+    entry.push(todoEntry);
+
+    //define saveTask function push it to entry
+    saveTask(entry)
+    
+    //retreiving usertask entry from localstorage
+    localStorage.getItem("usertask", JSON.stringify(entry));
+
+    });
+}
