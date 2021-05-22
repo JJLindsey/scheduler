1a1ca9c3ef6b21f103595fcda3f84f349a1b631f
JJLindsey <jlindsey010@gmail.com>
2021-05-22 15:16:51 -0500

    save persist storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,42 +1,17 @@
 
-//var interval = setInterval(hourUpdate, 15000);
+// get the data from local storage on page load
+var saveData = localStorage.getItem("userEntry");
+var parseData = JSON.parse(saveData);
+// select a textbox element using jQuery
+var targetTextBox = $('form-control');
+// change the value of the selected textbox to parseData
+targetTextBox.val(parseData)
+
 //day JS
 var displayDate = dayjs().format('dddd, MMMM.DD.YYYY');
   $(document).ready(function() {
     $("#currentDay").append(displayDate)
   });
-
-    
-// $(document).ready(function () {
-
-//   var myEntries = JSON.parse(localStorage.getItem('userEntry')) || [];
-
-//   // render saved data
-//   var allInputForms = $('.form-control')
-
-//   console.log(allInputForms)
-
-//   for (let i = 0; i < allInputForms.length; i++) {
-//     var currentInputForm = allInputForms[i];
-//     var myText = '';
-
-//     for (let j = 0; j < myEntries.length; j++) {
-//       var currentCustomData = $(currentInputForm).attr('custom-data');
-
-//       var currentEntry = myEntries[j];
-
-//       if (currentCustomData == currentEntry.hour) {
-//         myText = currentEntry.text;
-//       }
-//     }
-//   }
-
-//     $(currentInputForm).val(myText)
-// })
-// for (let i = 0; i < inputForms.length; i++) {
-//   var currentHour = parseInt(dayjs().format('H'));
-//  var inputHour = parseInt($(inputForms[i]).attr('data-hour'));
-// }
 
 
 //edit due dated in time block 
@@ -63,28 +38,16 @@ var inputForms = $(".form-control");
 };
 
 //event listener with local storage
-    //console.log(event);
+
   //save input to localstorage
 $(document).ready(function() {
     $('.btn').on('click', function(event) {
       event.preventDefault();
       var toDoEntry = $(this).closest("div.input-group").find("input[class~='form-control']").val();
+      // get the data-hour value number
+      var toDoHour = $(this).closest("div.input-group").find("input[class~='form-control']").attr('data-hour');
       console.log($(this).closest("div.input-group"));
-      localStorage.setItem("userEntry", JSON.stringify(toDoEntry))
+      localStorage.setItem("userEntry", JSON.stringify(toDoEntry));
       //console.log(toDoEntry);
       })
 })
-
-//color code time blocks in past, present and future with Moment.js
-
-
-
-//saved events persists even when page refreshed
-
-
-//listen for save button clicks
-//$("#save").on('click', function() {
-//get nearby values -look into siblings and parents relationship in jQuery
-//save in localStorage
-//Show notifications that item was saved to localStorage by adding class .show
-//Timeout to remove .show class after 5 seconds
